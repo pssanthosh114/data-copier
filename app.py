1cfa10daa12e56c308f6a5800382043ed966acca
Santhosh <Saisanthosh.Pullamaraju01@student.csulb.edu>
2022-06-26 00:12:32 -0700

    Read and Write logic is updated to Data Copier.

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-def main():
-    print('Hello World')
+import os
+import sys
+from read import get_json_reader
+from write import load_db_table
 
-if __name__=="__main__" :
+def process_table(BASE_DIR, conn, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in json_reader:
+        load_db_table(df, conn, table_name, df.columns[0])
+
+
+def main():
+    BASE_DIR = os.environ.get('BASE_DIR')
+    tables_names = sys.argv[1].split(',')
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+
+    for table_name in tables_names :
+        process_table(BASE_DIR, conn, table_name)
+
+
+
+
+
+if __name__=="__main__":
     main()
